97e9d7e79aa6a5d0fd95aeebf0c38f270f266ce1
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-10-12 19:06:57 +0200

    resize teaxtarea automatically

--- static/main.js
+++ static/main.js
@@ -1,5 +1,29 @@
 (function() {
+    var on = function(element, eventType, selector, fn) {
+        element.addEventListener(eventType, function(event) {
+            var target = event.target.closest(selector);
+            if (target && element.contains(target)) {
+                return fn.call(target, event);
+            }
+        });
+    };
+
     new SimpleMDE({
         spellChecker: false,
+    });
+
+    var resize = function(event) {
+        /* 0-timeout to get the already changed text */
+        setTimeout(() => {
+            this.style.height = 'auto';
+            this.style.height = this.scrollHeight + 5 + 'px';
+        }, 0);
+    };
+
+    on(document, 'init', 'textarea', resize);
+    on(document, 'change', 'textarea', resize);
+    on(document, 'keydown', 'textarea', resize);
+    document.querySelectorAll('textarea').forEach(function(e) {
+        resize.call(e);
     });
 })()
