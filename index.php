890f92ac8e277c61067149ae4123a19ce6f9e9bf
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-10-14 11:51:48 +0200

    wrapp stuff in a class

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
@@ -39,40 +39,6 @@ function validatePath($path)
     }
 }
 
-function getData($path, $raise=false)
-{
-    $data = array(
-        'yml' => '',
-        'md' => '',
-    );
-    if (file_exists("../_content$path/index.yml")) {
-        $data['yml'] = file_get_contents("../_content$path/index.yml");
-    }
-    if (file_exists("../_content$path/index.md")) {
-        $data['md'] = file_get_contents("../_content$path/index.md");
-    }
-    return $data;
-}
-
-function setData($path, $data)
-{
-    mkdirp("../_content$path");
-    file_put_contents("../_content$path/index.yml", $data['yml']);
-    file_put_contents("../_content$path/index.md", $data['md']);
-}
-
-function getSubpages($path)
-{
-    $p = "../_content$path";
-    $subpages = array();
-    foreach (scandir($p) as $name) {
-        if ($name !== '.' && $name !== '..' && is_dir("$p/$name")) {
-            $subpages[] = $name;
-        }
-    }
-    return $subpages;
-}
-
 function getBreadcrumbs($path)
 {
     $breadcrumbs = array('home' => '');
@@ -85,41 +51,116 @@ function getBreadcrumbs($path)
     return $breadcrumbs;
 }
 
-function render($path, $verbose=false)
+class Pupupu
 {
-    $parsedown = new Parsedown();
-    // $parsedown->setMarkupEscaped(true);
-    // $parsedown->setSafeMode(true);
-    $loader = new Twig_Loader_Filesystem('../_templates');
-    $twig = new Twig_Environment($loader);
+    public function __construct($srcDir, $targetDir)
+    {
+        $this->srcDir = $srcDir;
+        $this->targetDir = $targetDir;
 
-    if ($verbose) {
-        echo "rendering $path\n";
+        $this->parsedown = new Parsedown();
+        $loader = new Twig_Loader_Filesystem($targetDir . '/_templates');
+        $this->twig = new Twig_Environment($loader);
+
     }
 
-    $data = getData($path);
-    $template = $data['yml']['template'] ?? 'base.html';
-    $html = $twig->render($template, array(
-        'page' => Yaml::parse($data['yml']),
-        'site' => Yaml::parseFile("../_site/site.yml"),
-        'body' => $parsedown->text($data['md']),
-        'date' => time(),
-    ));
+    public function get($path, $name)
+    {
+        $p = $this->srcDir . '/_content' . $path . '/' . $name;
+        if (file_exists($p)) {
+            return file_get_contents($p);
+        } else {
+            return '';
+        }
+    }
 
-    mkdirp("..$path");
-    file_put_contents("..$path/index.html", $html);
-}
+    public function put($path, $name, $content)
+    {
+        $p = $this->srcDir . '/_content' . $path . '/' . $name;
+        mkdirp(dirname($p));
+        file_put_contents($p, $content);
+    }
 
-function renderAll($path='', $verbose=false)
-{
-    render($path, $verbose);
-    $dir = "../_content$path";
-    foreach (scandir($dir) as $name) {
-        if ($name !== '.' && $name !== '..' && is_dir("$dir/$name")) {
-            renderAll("$trimmed/$name", $verbose);
+    public function rm($path)
+    {
+        rrmdir($this->srcDir . '/_content' . $path);
+        rrmdir($this->targetDir . $path);
+    }
+
+    public function getBody($path)
+    {
+        return $this->parsedown->text($this->get($path, 'index.md'));
+    }
+
+    public function getPage($path)
+    {
+        return Yaml::parse($this->get($path, 'index.yml'));
+    }
+
+    public function getSite()
+    {
+        return Yaml::parse($this->get('', 'site.yml'));
+    }
+
+    public function getSubpages($path)
+    {
+        $p = $this->srcDir . '/_content' . $path;
+        return array_filter(scandir($p), function ($file) use ($p) {
+            return $file !== '.' && $file !== '..' && is_dir("$p/$file");
+        });
+    }
+
+    public function upload($file)
+    {
+        $p = $this->targetDir . '/uploads';
+        mkdirp($p);
+        move_uploaded_file($file['tmp_name'], $p . '/' . $file['name']);
+    }
+
+    public function getUploads()
+    {
+        $p = $this->targetDir . '/uploads';
+        return array_filter(scandir($p), function ($file) use ($p) {
+            return is_file("$p/$file");
+        });
+    }
+
+    public function rmUpload($name)
+    {
+        unlink($this->targetDir . '/uploads/' . $name);
+    }
+
+    public function render($path, $verbose=false)
+    {
+        if ($verbose) {
+            echo "rendering $path\n";
+        }
+
+        $page = $this->getPage($path);
+        $template = $page['template'] ?? 'base.html';
+        $html = $this->twig->render($template, array(
+            'page' => $page,
+            'site' => $this->getSite(),
+            'body' => $this->getBody($path),
+            'date' => time(),
+        ));
+
+        $filename = $page['filename'] ?? 'index.html';
+        $p = $this->targetDir . $path . '/' . $filename;
+        mkdirp(dirname($p));
+        file_put_contents($p, $html);
+    }
+
+    public function renderAll($verbose=false, $path='')
+    {
+        $this->render($path, $verbose);
+        foreach ($this->getSubpages($path) as $page) {
+            $this->renderAll($verbose, "$path/$page");
         }
     }
 }
+
+$pupupu = new Pupupu('..', '..');
 
 if (isset($_SERVER['REQUEST_METHOD'])) {
     $loader = new Twig_Loader_Filesystem('templates');
@@ -132,37 +173,33 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
     } elseif ($_GET['path'] === '_site') {
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             echo $twig->render('site.html', array(
-                'yml' => file_get_contents("../_site/site.yml"),
+                'yml' => $pupupu->get('', 'site.yml'),
             ));
         } else {
-            file_put_contents("../_site/site.yml", $_POST['yml']);
-            renderAll();
+            $pupupu->put('', 'site.yml', $_POST['yml']);
+            $pupupu->renderAll();
             header("Location: ", true, 302);
         }
     } elseif ($_GET['path'] === '_uploads') {
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             echo $twig->render('uploads.html', array(
-                'files' => array_filter(scandir('../uploads/'), function($file) {
-                    return is_file("../uploads/$file");
-                }),
+                'files' => $pupupu->getUploads(),
             ));
         } elseif (isset($_FILES['file'])) {
-            mkdirp('../uploads');
-            $f = $_FILES['file'];
-            move_uploaded_file($f['tmp_name'], '../uploads/' . $f['name']);
+            $pupupu->upload($_FILES['file']);
             header("Location: ", true, 302);
         } else {
-            unlink('../uploads/' . $_POST['file']);
+            $pupupu->rmUpload($_POST['file']);
             header("Location: ", true, 302);
         }
     } else {
         $path = validatePath($_GET['path']);
 
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            $data = getData($path);
             echo $twig->render('page.html', array(
-                'data' => $data,
-                'subpages' => getSubpages($path),
+                'yml' => $pupupu->get($path, 'index.yml'),
+                'md' => $pupupu->get($path, 'index.md'),
+                'subpages' => $pupupu->getSubpages($path),
                 'path' => $path,
                 'breadcrumbs' => getBreadcrumbs($path),
             ));
@@ -171,17 +208,16 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
                 http_response_code(400);
                 die();
             }
-            rrmdir("../_content$path");
-            rrmdir("..$path");
+            $pupupu->rm($path);
             $target = dirname($path);
             header("Location: ?path=$target", true, 302);
         } else {
-            // TODO validate form
-            setData($path, $_POST);
-            render($path);
+            $pupupu->put($path, 'index.yml', $_POST['yml']);
+            $pupupu->put($path, 'index.md', $_POST['md']);
+            $pupupu->render($path);
             header('Location: ', true, 302);
         }
     }
 } else {
-    renderAll('/', true);
+    $pupupu->renderAll(true);
 }
