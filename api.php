266206563c00a1f7b4517ec0cc9b9ec87d315acd
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-08-11 09:52:38 +0200

    require_once autoloader

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
 use Michelf\MarkdownExtra;
 
